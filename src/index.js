d8e411d822395a0b6074b5ea51aa0430964f1e0a
isar0se <itsr053@gmail.com>
2017-03-13 13:02:35 -0400

    switched history

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {Router, Route, browserHistory} from 'react-router'
+import {Router, Route, hashHistory} from 'react-router'
 import App from './App'
 
 render(
-	<Router history={browserHistory}>
+	<Router history={hashHistory}>
 		<Route path="/" component={App} />
 	</Router>,
 	document.getElementById('app')
